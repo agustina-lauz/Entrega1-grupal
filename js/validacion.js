13e2b868114228514b62b9b6907f853289867f2a
Martinacampos <110434913+Martinacampos@users.noreply.github.com>
2022-08-10 19:44:25 -0300

    condicion
    Aqui subo comentado el if de la password que no pudimos definir si es correcto o no

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,29 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+/*function verificarPasswords() {
+
+    pass1 = document.getElementById('password1');
+    pass2 = document.getElementById('password2');
+
+    if (pass1.value != pass2.value) {
+
+        // Si las constraseñas no coinciden mostramos un mensaje 
+        document.getElementById("alert-danger").classList.add("show");
+
+        return false;
+    }
+    
+    else {
+
+        // Mostramos un mensaje mencionando que las Contraseñas coinciden 
+        document.getElementById("alert-success").classList.add("show");
+
+       
+
+       
+        return true;
+    }
+
+} */
