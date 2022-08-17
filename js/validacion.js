ccd1eb5a15809bbb17679e14ef1220b9c4279b4c
agustinalauz <agustinalauz81@gmail.com>
2022-08-17 19:52:28 -0300

    improvment functions

--- js/validacion.js
+++ js/validacion.js
@@ -12,7 +12,6 @@ let email = document.getElementById("email")
 let pass1 = document.getElementById("password1")
 let pass2 = document.getElementById("password2")
 let terminos = document.getElementById("terminos")
-let fromGroup = document.getElementsByClassName("datos")
 let button = document.getElementById("regBtn")
 
 
@@ -20,29 +19,18 @@ function validaDatos(){
     return nombre.value.length > 0 && apellido.value.length > 0 && email.value.length > 0
 
 }
- 
-
 
 function validaPass(){
-    let pas1 = document.getElementById('password1').value;
-    let pas2 = document.getElementById('password2').value;
-    return (pas1 === pas2);
+    return (pass1.value === pass2.value);
 }
-
-
 
 function validaCaracteresPas(){
-    return document.getElementById('password1').value.length > 5;
+    return pass1.value.length > 5;
 }
-
-
 
 function check() {
-    return document.getElementById("terminos").checked;
+    return terminos.checked;
 }
-
-
-
 
  button.addEventListener("click", e =>
     {
